aae89d11a9a55763f990e2e70876ca993492d1a7
Max <maxdejong6@gmail.com>
2020-10-18 19:43:11 +1100

    ConnectDB

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+connectDB();
+
+app.use(express.json({ extended: false }));
 
 app.listen(PORT, console.log(`Running on port ${PORT}`));
